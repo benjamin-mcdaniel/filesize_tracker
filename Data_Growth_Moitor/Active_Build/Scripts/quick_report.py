d2f37ab1df9a0fb9dcaca740e03214f106443589
benjamin-mcdaniel <benjamin.f.mcdaniel@gmail.com>
2019-06-15 14:14:30 -0700

    x

--- Data_Growth_Moitor/Active_Build/Scripts/quick_report.py
+++ Data_Growth_Moitor/Active_Build/Scripts/quick_report.py
@@ -1,20 +1,51 @@
 import os
 import csv
-print ('hello')
-folders = [x[0] for x in os.walk(os.getcwd())]
-print (folders)
-file1 = os.getcwd() + '\\test.csv'
-print (file1)
-Fields = ['Folder','File_Count','Folder_Count']
-size = 0
-path = '.'
+import glob
+
+
+print (' -------------- Starting --------------')
+os.chdir(r"C:\Users\benja\Source\Repos\sandbox")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#folders = [x[0] for x in os.walk(os.getcwd())]
+#print (folders)
+
+
+#current_dir = os.getcwd() + '\\test.csv'
+#print (current_dir)
+
+#Fields = ['Folder','File_Count','Folder_Count']
+
+
+
+
+
 #for path, dirs, files in os.walk(start_path):
 #    for f in files:
 #       fp = os.path.join(path, f)
 
 
-with open(file1, 'w', newline='', ) as csvfile:
-    csvwriter = csv.writer(csvfile)
-    
-    csvwriter.writerows([Fields])
-    csvwriter.writerows([folders])
+#with open(current_dir, 'w', newline='\n', ) as csvfile:
+#    csvwriter = csv.writer(csvfile)
+#    
+#    csvwriter.writerows([Fields])
+#    csvwriter.writerows([folders])
