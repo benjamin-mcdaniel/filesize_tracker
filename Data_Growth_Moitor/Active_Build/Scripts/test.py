71405bd255b8856b1608bb775eb9b08ab03ce496
benjamin-mcdaniel <benjamin.f.mcdaniel@gmail.com>
2019-07-01 00:01:44 -0700

    z

--- Data_Growth_Moitor/Active_Build/Scripts/test.py
+++ Data_Growth_Moitor/Active_Build/Scripts/test.py
@@ -1,4 +1,5 @@
 import os
+import panda
 from os import walk
 # from os.path import join, getsize
 from os import walk
@@ -6,6 +7,8 @@ from os.path import join, getsize
 path = os.getcwd()
 os.chdir(r"C:\Users\benja\Source\Repos\sandbox")
 
+
+dataframe = panda.dataframe(columns=['row', 'path', 'folder_name', 'folder_size', 'tree_size', 'total_tree_size', 'last_edit_time', 'created_time', 'tree_file_count', 'tree_folder_count'])
 
 def get_size(start_path = '.'):
     total_size = 0
@@ -20,24 +23,39 @@ def get_size(start_path = '.'):
 
 print(' -------------- imports done --------------')
 
-
 current_target = os.walk(os.getcwd())
 
 print(' -------------- set directory --------------')
 list_folders = []
 for root, dirs, files in os.walk(os.getcwd(), topdown=False):
     list_folders.append(root)
-    print(root)
-    for f in list_folders:
-        folder_size = 0
-        tree_size = 0
-        total_tree_size = 0
-        print("Folder:", f)
-        folder_size = getsize(f)
-        tree_size = get_size(start_path=f)
-        total_tree_size = folder_size + tree_size
-        print("size:", folder_size, "tree size:", total_tree_size)
-        print(' ----------------------------')
+
+    row = 0
+for folder in list_folders:
+    row += 1
+    dataframe_row = []
+    folder_size = 0
+    tree_size = 0
+    total_tree_size = 0
+    last_edit_time = 0
+    created_time = 0
+    tree_file_count = 0
+    tree_folder_count = 0
+    tree_folder_count = sum([len(dirs) for r, d, files in os.walk(folder)])
+    tree_file_count = sum([len(files) for r, d, files in os.walk(folder)])
+    created_time = os.path.getctime(folder)
+    last_edit_time = os.path.getmtime(folder)
+    print("Folder:", folder)
+    folder_size = getsize(folder)
+    tree_size = get_size(start_path=folder)
+    total_tree_size = folder_size + tree_size
+    print("folder size:", folder_size, "sub_tree size:", tree_size, "tree size:", total_tree_size, "file count:", tree_file_count, "folder count:", tree_folder_count)
+    print("folder last edit:", last_edit_time, "Folder Created:", created_time)
+    dataframe_row= ({'row' : row, 'path' : folder , 'folder_size' : folder_size, 'tree_size' : tree_size , 'total_tree_size' : total_tree_size , 'last_edit_time' : last_edit_time , 'created_time' : created_time, 'tree_file_count' : tree_file_count, 'tree_folder_count' : tree_folder_count})
+    dataframe_row
+    print(' ----------------------------')
+
+
 print(root)
 
 
